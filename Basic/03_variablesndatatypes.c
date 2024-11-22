3a5f8d88f8bcb72e71083ec1efeb714c27f702a7
Shubham01405 <shubhamkumarsahu6080@gmail.com>
2024-11-22 19:51:32 +0530

    Initial commit

---
+++ Basic/03_variablesndatatypes.c
@@ -0,0 +1,29 @@
+// Variables
+
+#include<stdio.h>
+
+int main(){
+    // int a; ---------->  ( %d )
+    // 4bytes (32bits)
+    // a = 1;
+    int a;
+    a = 1;
+
+    // float b = 1.4; -----> ( %f )
+    // 4bytes (32bits)
+    float b;
+    b = 1.4;
+
+    // char c ='a'; --------> ( %c )
+    // 1byte (8bits)
+    char c;
+    c = 'a';
+    
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+
+
+}
+
